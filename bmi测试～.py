03582725152e403725e43028e4bb3c9b16ec70b7
orangeJJJ <35887455+orangeJJJ@users.noreply.github.com>
2018-01-29 13:34:11 +0800

    Add files via upload

---
+++ bmi测试～.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+s=input('please enter your height (m.):')
+z=input('please enter your weight (kg.):')
+height=float(s)
+weight=float(z)
+bmi=weight/(height**2)
+if bmi<18.5:
+    print('bmi指数:%.2f;你的体重过轻，多吃点'%bmi)
+elif 25>=bmi>18.5:
+    print('bmi指数:%.2f;你的体重正常'%bmi)
+elif 28>=bmi>25:
+    print('bmi指数:%.2f;你的体重过重，注意啦'%bmi)
+elif 32>=bmi>28:
+    print('bmi指数:%.2f;你的体重肥胖，控制'%bmi)
+else:
+    print('bmi指数:%.2f;严重肥胖，节制'%bmi)
