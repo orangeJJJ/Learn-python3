d7015cc27eb37e994017d569cf113a2ecac23047
orangeJJJ <35887455+orangeJJJ@users.noreply.github.com>
2018-01-30 19:02:55 +0800

    Update 解一元二次方程def_func.py

--- 解一元二次方程def_func.py
+++ 解一元二次方程def_func.py
@@ -12,7 +12,7 @@ def quadratic(a,b,c):
     if b*b-4*a*c<0:
         return '无解'
     else:
-        x1=(-b+math.sqrt(b*b-4*a*c))/2*a
+        x1=(-b+math.sqrt(b*b-4*a*c))/2*a    #计算平方根可以调用math.sqrt()函数：
         x2=(-b-math.sqrt(b*b-4*a*c))/2*a
         return x1,x2
 a=float(input('请输入一元二次方程中的a:'))
