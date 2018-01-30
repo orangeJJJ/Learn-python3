5a71c8b83df57bdac48097fbff3b0da560cc23c1
orangeJJJ <35887455+orangeJJJ@users.noreply.github.com>
2018-01-30 18:57:17 +0800

    Add files via upload

---
+++ 解一元二次方程do_func.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import math
+def quadratic(a,b,c):
+    if not isinstance(a, (int, float)):
+        raise TypeError('bad operand type')
+    if not isinstance(b, (int, float)):
+        raise TypeError('bad operand type')
+    if not isinstance(c, (int, float)):
+        raise TypeError('bad operand type')
+    if b*b-4*a*c<0:
+        return '无解'
+    else:
+        x1=(-b+math.sqrt(b*b-4*a*c))/2*a
+        x2=(-b-math.sqrt(b*b-4*a*c))/2*a
+        return x1,x2
+a=float(input('请输入一元二次方程中的a:'))
+b=float(input('请输入一元二次方程中的b:'))
+c=float(input('请输入一元二次方程中的c:'))
+print(quadratic(a,b,c))
