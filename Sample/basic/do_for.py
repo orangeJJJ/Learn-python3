159849a0b23cd74513957209513bf025c27a25aa
orangeJJJ <35887455+orangeJJJ@users.noreply.github.com>
2018-02-04 17:22:02 +0800

    Update and rename 字符串连接方式_do_for.py to do_for.py

--- Sample/basic/do_for.py
+++ Sample/basic/do_for.py
@@ -6,7 +6,7 @@
 # 2.两个字符串用“逗号”隔开(字符串之间会多出一个空格)
 # 3.符号“%”连接一个字符串和一组变量,自动用右边变量组中的变量替换
 
-L=['bart','Lisa','Adam']
+L = ['bart','Lisa','Adam']
 for name in L:
     print('hello,'+name+'!')
 for name in L:
