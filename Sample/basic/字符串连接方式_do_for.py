3cee85a212675714c887852724524e06b38bcda9
orangeJJJ <35887455+orangeJJJ@users.noreply.github.com>
2018-02-04 17:08:09 +0800

    Add files via upload

---
+++ Sample/basic/字符串连接方式_do_for.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#   字符串连接方式:
+# 1.直接用 “+” 来连接两个字符串
+# 2.两个字符串用“逗号”隔开(字符串之间会多出一个空格)
+# 3.符号“%”连接一个字符串和一组变量,自动用右边变量组中的变量替换
+
+L=['bart','Lisa','Adam']
+for name in L:
+    print('hello,'+name+'!')
+for name in L:
+    print('hello,%s!'%name)
+for name in L:
+    print('hello,',name,'!')
